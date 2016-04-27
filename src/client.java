13a3e60ca86b0e5d39085749c4f78f24f7d47f1e
pkprajapat <prajapatpankajkumar@gmail.com>
2016-04-27 21:22:32 +0530

    across systems over wifi networking works. combine

--- src/client.java
+++ src/client.java
@@ -1,110 +1,98 @@
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-import java.net.*;
-import java.io.*;
+public class client implements Runnable {
 
-class clientOne extends Thread {
-    private Socket socket;
-    private BufferedReader in;
-    private PrintWriter out;
-    private static int counter = 0;
-    public clientOne (InetAddress addr , int port) {
-        System.out.println("Making client ");
-        try {
-            socket = new Socket(addr, port);
-        } catch(IOException e) {
-            // If the creation of the socket fails, nothing needs to be cleaned up
-        }
-        try {   
-            in = new BufferedReader(
-                    new InputStreamReader(
-                        socket.getInputStream()));
-            // Enable auto-flush:
-            out = new PrintWriter(
-                    new BufferedWriter(
-                        new OutputStreamWriter(
-                            socket.getOutputStream())), true);
-            start();
-        } catch(IOException e) {
-            // The socket should be closed on any failures other than the socket constructor:
-            try {
-                socket.close();
-            } catch(IOException e2) {}
-        }
-        // Otherwise the socket will be closed by the run() method of the thread.
+  // The client socket
+  private static Socket clientSocket = null;
+  // The output stream
+  private static PrintStream os = null;
+  // The input stream
+  private static DataInputStream is = null;
+
+  private static BufferedReader inputLine = null;
+  private static boolean closed = false;
+  
+  public static void main(String[] args) {
+
+    // The default port.
+    int portNumber = 2222;
+    // The default host.
+    String host = "localhost";
+
+    if (args.length < 2) {
+      System.out
+          .println("Usage: java client <host> <portNumber>\n"
+              + "Now using host=" + host + ", portNumber=" + portNumber);
+    } else {
+      host = args[0];
+      portNumber = Integer.valueOf(args[1]).intValue();
     }
-    public void run() {
-        try {
-            //for(int i = 0; i < 25; i++) {
-        	int i = 0;
-        	while(true){ i++;
-                String str = in.readLine();
-                System.out.println(str);
-            }
-            //out.println("END");
-        } catch(IOException e) {
-        } finally {
-            // Always close it:
-            try {
-                socket.close();
-            } catch(IOException e) {}
-            
+
+    /*
+     * Open a socket on a given host and port. Open input and output streams.
+     */
+    try {
+      clientSocket = new Socket(host, portNumber);
+      inputLine = new BufferedReader(new InputStreamReader(System.in));
+      os = new PrintStream(clientSocket.getOutputStream());
+      is = new DataInputStream(clientSocket.getInputStream());
+    } catch (UnknownHostException e) {
+      System.err.println("Don't know about host " + host);
+    } catch (IOException e) {
+      System.err.println("Couldn't get I/O for the connection to the host "
+          + host);
+    }
+
+    /*
+     * If everything has been initialized then we want to write some data to the
+     * socket we have opened a connection to on the port portNumber.
+     */
+    if (clientSocket != null && os != null && is != null) {
+      try {
+
+        /* Create a thread to read from the server. */
+        new Thread(new client()).start();
+        while (!closed) {
+          os.println(inputLine.readLine().trim());
         }
-    }
-}
-
-public class client {
-    //static final int max_clients = 4;
-    public static void main(String[] args)
-            throws IOException, InterruptedException {
-        InetAddress addr = InetAddress.getByName(args[0]);;
-        int port = Integer.parseInt(args[1]);
-        new clientOne(addr , port);
-        /*while(true) {
-            if(clientOne.threadCount()
-                 < max_clients)
-                new clientOne(addr , port);
-            Thread.currentThread().sleep(100);
-        }*/
-    }
-}
-
-/*
-public class client
-{
-   public static void main(String [] args)
-   {
-      String serverName = args[0];
-      int port = Integer.parseInt(args[1]);
-       
-      try
-      {
-         System.out.println("Connecting to " + serverName +
-		 " on port " + port);
-         
-         Socket client = new Socket(serverName, port);
-         
-         System.out.println("Just connected to " 
-		 + client.getRemoteSocketAddress());
-         
-         OutputStream outToServer = client.getOutputStream();
-         
-         DataOutputStream out = new DataOutputStream(outToServer);
-         InputStream inFromServer = client.getInputStream();
-         DataInputStream in =
-                 new DataInputStream(inFromServer);
-         out.writeUTF("Hello from "
-                      + client.getLocalSocketAddress());
-         for (int i = 0; i<10;i++){
-        	 out.writeUTF(Integer.toString(i));
-	  	}
-         for (int i = 0;i<10;i++){
-        	 System.out.println("Server says " + in.readUTF());
-         }
-         client.close();
-      }catch(IOException e)
-      {
-         e.printStackTrace();
+        /*
+         * Close the output stream, close the input stream, close the socket.
+         */
+        os.close();
+        is.close();
+        clientSocket.close();
+      } catch (IOException e) {
+        System.err.println("IOException:  " + e);
       }
-   }
+    }
+  }
+
+  /*
+   * Create a thread to read from the server. (non-Javadoc)
+   * 
+   * @see java.lang.Runnable#run()
+   */
+  public void run() {
+    /*
+     * Keep on reading from the socket till we receive "Bye" from the
+     * server. Once we received that then we want to break.
+     */
+    String responseLine;
+    try {
+      while ((responseLine = is.readLine()) != null) {
+        System.out.println(responseLine);
+        if (responseLine.indexOf("*** Bye") != -1)
+          break;
+      }
+      closed = true;
+    } catch (IOException e) {
+      System.err.println("IOException:  " + e);
+    }
+  }
 }
-*/
--- src/server.java
+++ src/server.java
@@ -1,96 +1,202 @@
-import java.net.*;
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
 
-class serverOne extends Thread
-{
-   private ServerSocket serverSocket;
-   private Socket socket;
-   private BufferedReader in;
-   private PrintWriter out;
-   public serverOne(Socket s) throws IOException {
-       socket = s;
-       in = new BufferedReader(
-               new InputStreamReader(
-                   socket.getInputStream()));
-       out = new PrintWriter(
-               new BufferedWriter(
-                   new OutputStreamWriter(
-                       socket.getOutputStream())), true);
-       start(); // Calls run()
-   }
-   
-   public void run()
-   {
-	   try {
-           while (true) { 
-               String input = in.readLine();
-               if (input.equals("END")) break;
-               System.out.println("Echoing: " + input);
-               out.println(input);
-           }
-           System.out.println("closing...");
-       } catch (IOException e) {
-       } finally {
-           try {
-               socket.close();
-           } catch(IOException e) {}
-       }
-   }
-}
-   /*
-   while(true)
-   {
-      try
-      {
-         System.out.println("Waiting for client on port " +
-         serverSocket.getLocalPort() + "...");
-         Socket server = serverSocket.accept();
-         System.out.println("Just connected to "
-               + server.getRemoteSocketAddress());
-         DataInputStream in =
-                 new DataInputStream(server.getInputStream());
-         DataOutputStream out =
-                 new DataOutputStream(server.getOutputStream());
-            out.writeUTF("Thank you for connecting to "
-              + server.getLocalSocketAddress() + "\nGoodbye!");
-         for(int i = 0;i<10;i++){
-	            String received = in.readUTF();
-	            if(received != null){
-		            System.out.println(received);
-		            out.writeUTF("received packet" + (received));
-	            }
-         }
-         server.close();
-      }catch(SocketTimeoutException s)
-      {
-         System.out.println("Socket timed out!");
-         break;
-      }catch(IOException e)
-      {
-         e.printStackTrace();
-         break;
-      }
-   }
-   */
-public class server {    
-   public static void main(String [] args) throws IOException
-   {
-	   int clients = 0;			// number of clients connected to the server
-	   int port = Integer.parseInt(args[0]);
-	   ServerSocket s = new ServerSocket(port);
-       try
-       {
-    	  while(true && clients < 4) {
-              Socket socket = s.accept();
-              clients++;
-              try {
-                  new serverOne(socket);
-              } catch(IOException e) {
-                  socket.close();
-              }
+/*
+ * A chat server that delivers public and private messages.
+ */
+public class server {
+
+  // The server socket.
+  private static ServerSocket serverSocket = null;
+  // The client socket.
+  private static Socket clientSocket = null;
+
+  // This chat server can accept up to maxClientsCount clients' connections.
+  private static final int maxClientsCount = 4;
+  private static final clientThread[] threads = new clientThread[maxClientsCount];
+
+  public static void main(String args[]) {
+
+    // The default port number.
+    int portNumber = 2222;
+    if (args.length < 1) {
+      System.out.println("Usage: java server <portNumber>\n"
+          + "Now using port number=" + portNumber);
+    } else {
+      portNumber = Integer.valueOf(args[0]).intValue();
+    }
+
+    /*
+     * Open a server socket on the portNumber (default 2222). Note that we can
+     * not choose a port less than 1023 if we are not privileged users (root).
+     */
+    try {
+      serverSocket = new ServerSocket(portNumber);
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+
+    /*
+     * Create a client socket for each connection and pass it to a new client
+     * thread.
+     */
+    while (true) {
+      try {
+        clientSocket = serverSocket.accept();
+        int i = 0;
+        for (i = 0; i < maxClientsCount; i++) {
+          if (threads[i] == null) {
+            (threads[i] = new clientThread(clientSocket, threads)).start();
+            break;
           }
-       } finally {
-          s.close();
-       }
-   }
+        }
+        if (i == maxClientsCount) {
+          PrintStream os = new PrintStream(clientSocket.getOutputStream());
+          os.println("Server too busy. Try later.");
+          os.close();
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println(e);
+      }
+    }
+  }
+}
+
+/*
+ * The chat client thread. This client thread opens the input and the output
+ * streams for a particular client, ask the client's name, informs all the
+ * clients connected to the server about the fact that a new client has joined
+ * the chat room, and as long as it receive data, echos that data back to all
+ * other clients. The thread broadcast the incoming messages to all clients and
+ * routes the private message to the particular client. When a client leaves the
+ * chat room this thread informs also all the clients about that and terminates.
+ */
+class clientThread extends Thread {
+
+  private String clientName = null;
+  private DataInputStream is = null;
+  private PrintStream os = null;
+  private Socket clientSocket = null;
+  private final clientThread[] threads;
+  private int maxClientsCount;
+
+  public clientThread(Socket clientSocket, clientThread[] threads) {
+    this.clientSocket = clientSocket;
+    this.threads = threads;
+    maxClientsCount = threads.length;
+  }
+
+  public void run() {
+    int maxClientsCount = this.maxClientsCount;
+    clientThread[] threads = this.threads;
+
+    try {
+      /*
+       * Create input and output streams for this client.
+       */
+      is = new DataInputStream(clientSocket.getInputStream());
+      os = new PrintStream(clientSocket.getOutputStream());
+      String name;
+      while (true) {
+        os.println("Enter your name.");
+        name = is.readLine().trim();
+        if (name.indexOf('@') == -1) {
+          break;
+        } else {
+          os.println("The name should not contain '@' character.");
+        }
+      }
+
+      /* Welcome the new the client. */
+      os.println("Welcome " + name
+          + " to our chat room.\nTo leave enter /quit in a new line.");
+      synchronized (this) {
+        for (int i = 0; i < maxClientsCount; i++) {
+          if (threads[i] != null && threads[i] == this) {
+            clientName = "@" + name;
+            break;
+          }
+        }
+        for (int i = 0; i < maxClientsCount; i++) {
+          if (threads[i] != null && threads[i] != this) {
+            threads[i].os.println("*** A new user " + name
+                + " entered the chat room !!! ***");
+          }
+        }
+      }
+      /* Start the conversation. */
+      while (true) {
+        String line = is.readLine();
+        if (line.startsWith("/quit")) {
+          break;
+        }
+        /* If the message is private sent it to the given client. */
+        if (line.startsWith("@")) {
+          String[] words = line.split("\\s", 2);
+          if (words.length > 1 && words[1] != null) {
+            words[1] = words[1].trim();
+            if (!words[1].isEmpty()) {
+              synchronized (this) {
+                for (int i = 0; i < maxClientsCount; i++) {
+                  if (threads[i] != null && threads[i] != this
+                      && threads[i].clientName != null
+                      && threads[i].clientName.equals(words[0])) {
+                    threads[i].os.println("<" + name + "> " + words[1]);
+                    /*
+                     * Echo this message to let the client know the private
+                     * message was sent.
+                     */
+                    this.os.println(">" + name + "> " + words[1]);
+                    break;
+                  }
+                }
+              }
+            }
+          }
+        } else {
+          /* The message is public, broadcast it to all other clients. */
+          synchronized (this) {
+            for (int i = 0; i < maxClientsCount; i++) {
+              if (threads[i] != null && threads[i].clientName != null) {
+                threads[i].os.println("<" + name + "> " + line);
+              }
+            }
+          }
+        }
+      }
+      synchronized (this) {
+        for (int i = 0; i < maxClientsCount; i++) {
+          if (threads[i] != null && threads[i] != this
+              && threads[i].clientName != null) {
+            threads[i].os.println("*** The user " + name
+                + " is leaving the chat room !!! ***");
+          }
+        }
+      }
+      os.println("*** Bye " + name + " ***");
+
+      /*
+       * Clean up. Set the current thread variable to null so that a new client
+       * could be accepted by the server.
+       */
+      synchronized (this) {
+        for (int i = 0; i < maxClientsCount; i++) {
+          if (threads[i] == this) {
+            threads[i] = null;
+          }
+        }
+      }
+      /*
+       * Close the output stream, close the input stream, close the socket.
+       */
+      is.close();
+      os.close();
+      clientSocket.close();
+    } catch (IOException e) {
+    }
+  }
 }
